362f356aec0aafce31fb6cdf8edd4fa5800dc590
DUCDEPTRAIKHOAITO <minhducvo140201@gmail.com>
2025-05-16 21:45:47 +0700

    update token

---
+++ src/main/java/org/elearning/dto/elearning/auth/RegisterRequest.java
@@ -0,0 +1,10 @@
+package org.elearning.dto.elearning.auth;
+
+import lombok.Data;
+
+@Data
+public class RegisterRequest {
+    private String name;
+    private String email;
+    private String password;
+}
